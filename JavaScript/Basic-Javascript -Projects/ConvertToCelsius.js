0bb7ec4ad4ed9a05e0718deccddd18ea28496a1b
Aaron Frichtl <aaronfrichtl@gmail.com>
2019-06-06 17:39:22 -0700

    Javascript Projects
    Various demonstrations of JS in action.

---
+++ JavaScript/Basic-Javascript -Projects/ConvertToCelsius.js
@@ -0,0 +1,13 @@
+function functionName(argument1, argument2) {
+    // function body
+    return result1, result2;
+}
+
+function convertToCelsius(tempInF) {
+    // function to convert temperature
+    // from Fahrenheit to Celsius
+    tempInC = ( tempInF - 32 ) * 5/9;
+    return tempInC
+}
+
+console.log( convertToCelsius (212) );
---
+++ JavaScript/Basic-Javascript -Projects/SwitchBlock.js
@@ -0,0 +1,17 @@
+var fruit = "Orange";
+switch (fruit){
+    case "Banana":
+    console.log("Bananas are yellow.");
+    break;
+    case "Orange":
+    console.log("Oranges are orange.");
+    break;
+    case "Strawberry":
+    console.log("Strawberries are red.");
+    break;
+    case "Lime":
+    console.log("Limes are green.");
+    break;
+    default:
+    console.log("Sorry, I don't know what colot that fruit is.");
+}
